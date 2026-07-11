5b329f0231346eb02fabf480419c93a44ae52b5e
WaterRun <linzhangrun49@gmail.com>
2026-07-11 13:41:01 +0800

    chore: ignore non-product trees and add XP IE6 SAMPLE.bmp
    Untrack Tests, docs, and tools (keep them gitignored), and document a
    fullscreen XP IE6 MockLocal chat screenshot after the README title.

--- tools/xp/check_display.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-dir /b "C:\Program Files\VMware\VMware Tools"
-echo ---DPI---
-reg query "HKCU\Control Panel\Desktop" /v LogPixels
-reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\FontDPI" /v LogPixels
-reg query "HKLM\SYSTEM\CurrentControlSet\Hardware Profiles\Current\Software\Fonts" /v LogPixels
-echo ---RES---
-wmic path Win32_VideoController get CurrentHorizontalResolution,CurrentVerticalResolution /value
